e2eab81f7a50cb0e525bea40100fb3b05db65d11
kurosakananako <kurosakananako@localhost>
2020-05-20 01:35:07 +0900

    黒阪菜々子

--- 1_1/src/SukiFood.java
+++ 1_1/src/SukiFood.java
@@ -3,6 +3,9 @@ public class SukiFood {
 	public static void main(String[] args) {
 		System.out.println("私の好きな食べ物は寿司です。");
 		System.out.println("なぜなら美味しいからです。");	
+		
+		System.out.println("バナナのおすすめの食べ方はありますか？");
+		
 	}
 
 }
